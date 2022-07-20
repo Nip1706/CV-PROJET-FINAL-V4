ea6c73ff147df859e3aae1d918adccd7f1e89e28
Nip1706 <107402012+Nip1706@users.noreply.github.com>
2022-07-20 21:52:55 +0200

    Add files via upload

---
+++ scriptcv.js
@@ -0,0 +1,57 @@
+let p1 = document.getElementById("appctc");
+let p2 = document.getElementById("appqp");
+let p3 = document.getElementById("appcom");
+let a1 = document.getElementById("croix");
+
+a1.addEventListener('click', function(){
+    p1.style.visibility = "hidden"
+})
+
+let a2 = document.getElementById("ctc");
+
+a2.addEventListener('click', function(){
+    p1.style.visibility = "visible"
+})
+
+let a3 = document.getElementById("qp");
+
+a3.addEventListener('click', function(){
+    p2.style.visibility = "visible"
+})
+let a4 = document.getElementById("croix2");
+
+a4.addEventListener('click', function(){
+    p2.style.visibility = "hidden"
+})
+
+let a5 = document.getElementById("com");
+
+
+
+let a6 = document.getElementById("croix3");
+
+
+
+let a7 = document.getElementById("logohtml");
+var a8 = document.getElementById("html");
+
+a7.addEventListener('click', function(){
+    a8.style.animation = "running"
+    a7b.style.animation = "running"
+
+})
+
+let a7b = document.getElementById("logohtml1");
+let a9 = document.getElementById("logocss");
+let a9b = document.getElementById("logocss1");
+let a10 = document.getElementById("centcss");
+let a11 = document.getElementById("logojs");
+let a11b = document.getElementById("logojs1");
+let a12 = document.getElementById("centjs");
+let a13 = document.getElementById("logogithub");
+let a13b = document.getElementById("logogithub1");
+let a14 = document.getElementById("centgit");
+let a15 = document.getElementById("skills");
+
+
+
